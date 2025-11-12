63908224d150a7fe8b83e5a4332e7fa95f36ae62
Aakash Dammala <dammala.aakash@gmail.com>
2025-11-11 22:14:20 -0500

    Task1 - Update the publisher to be an Oven 1. Updated the print statements for the publisher to act as an Oven 2. Updated the node name

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -3,40 +3,57 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <string>
 
 using namespace std::chrono_literals;
 
-class MinimalPublisher : public rclcpp::Node
+class OvenPublisher : public rclcpp::Node
 {
  public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0)
+  OvenPublisher() : Node("oven_publisher"), time_rem_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    publisher_ = this->create_publisher<std_msgs::msg::String>("oven_status", 10);
+    timer_ = this->create_wall_timer(500ms, std::bind(&OvenPublisher::timer_callback, this));
+    RCLCPP_DEBUG_STREAM(this->get_logger(), "The oven has been powered up!");
   }
 
  private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data =
-        "Hello terrapins, I am Aakash, welcome to the world of turtles. Terrapin count: " +
-        std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
+    if (time_rem_ <= 0)
+    {
+      std::string msg = "The Oven is now ready to use now";
+      RCLCPP_INFO_STREAM(this->get_logger(), msg);
+
+      std_msgs::msg::String pub_msg;
+      pub_msg.data = msg;
+      publisher_->publish(pub_msg);
+    }
+    else
+    {
+      int time_rem = 0;
+      std::string msg =
+          "The Oven is running, time remaining: " + std::to_string(time_rem) + " seconds";
+      RCLCPP_INFO_STREAM(this->get_logger(), msg);
+
+      std_msgs::msg::String pub_msg;
+      pub_msg.data = msg;
+      publisher_->publish(pub_msg);
+    }
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  int time_rem_;
 };
 
 int main(int argc, char* argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<OvenPublisher>());
   rclcpp::shutdown();
   return 0;
 }
